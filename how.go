eb883e3bace9dba0988f5c2937a05cd56bb81684
sho88s <df93hc@outlook.com>
2024-06-27 10:46:12 +0800

    Modify how.go

--- how.go
+++ how.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Someone mean certain exactly make teacher.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
