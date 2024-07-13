ab8c89d1bc76ba7ac25248b6adfb364d07c7fb65
sho88s <df93hc@outlook.com>
2024-07-13 14:22:36 +0800

    Modify they.cpp

--- they.cpp
+++ they.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Right cold arrive child candidate record.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
