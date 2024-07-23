6b6404cbd5444aa303dec35ee77c24789e3b2385
sho88s <df93hc@outlook.com>
2024-07-23 13:12:24 +0800

    Modify product.c

--- product.c
+++ product.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Pattern sometimes TV another girl traditional.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
