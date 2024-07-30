db93e6a495271fa8bddf2b8336ffcb614165b9c3
sho88s <df93hc@outlook.com>
2024-07-30 15:06:54 +0800

    Modify explain.cpp

--- explain.cpp
+++ explain.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Out speech your wife.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
