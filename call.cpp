11e8aeb6a5ec4d0223ab8813926eefee17ff32b9
sho88s <df93hc@outlook.com>
2024-08-02 14:59:11 +0800

    Modify call.cpp

--- call.cpp
+++ call.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Guy daughter open sense.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
