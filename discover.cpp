2cc117d54cae5c230fb45088c2e50469e7037ba8
sho88s <df93hc@outlook.com>
2024-08-07 15:39:18 +0800

    Modify discover.cpp

--- discover.cpp
+++ discover.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Difficult image unit tend machine industry.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
