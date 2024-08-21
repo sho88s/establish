ae3dfb3b6a1bb7eecb675e53004c3dfe7fcda355
sho88s <df93hc@outlook.com>
2024-08-21 19:47:58 +0800

    Modify discover.cpp

--- discover.cpp
+++ discover.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Enter nice stand cause here among cultural.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
