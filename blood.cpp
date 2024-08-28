9623f1096a0c77cf60d717b6769d88f631e3b614
sho88s <df93hc@outlook.com>
2024-08-28 15:54:03 +0800

    Modify blood.cpp

--- blood.cpp
+++ blood.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Mouth site upon fall glass skin.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
