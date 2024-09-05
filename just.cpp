30c50abe3100efb6fb84d63b6409f87e7e8d93dd
sho88s <df93hc@outlook.com>
2024-09-05 11:56:21 +0800

    Modify just.cpp

--- just.cpp
+++ just.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Product here read level there investment.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
