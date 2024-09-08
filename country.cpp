668b89487a169deb13c020e8fdb7c91a2430c03a
sho88s <df93hc@outlook.com>
2024-09-08 13:59:38 +0800

    Modify country.cpp

--- country.cpp
+++ country.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Better medical trade line.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
