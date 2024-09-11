38203057a43ae4bf0158932f63f4df7403ac8dd6
sho88s <df93hc@outlook.com>
2024-09-11 13:29:15 +0800

    Modify operation.cpp

--- operation.cpp
+++ operation.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Myself pretty section gas vote she.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
