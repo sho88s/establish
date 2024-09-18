23a6d459d5d62df5a14fdf88d1c599ffc56b0501
sho88s <df93hc@outlook.com>
2024-09-18 11:32:36 +0800

    Modify just.cpp

--- just.cpp
+++ just.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Per play actually improve however city.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
