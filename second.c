4d2886c3c26a5a0fd0965954dc8a28084b1009b9
sho88s <df93hc@outlook.com>
2024-10-12 10:04:57 +0800

    Modify second.c

--- second.c
+++ second.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Sit outside hard politics environmental last.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
