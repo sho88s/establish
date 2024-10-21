1adb4cc7958d9df3a8516d8459ca59b7d88fc4c5
sho88s <df93hc@outlook.com>
2024-10-21 11:31:20 +0800

    Modify think.c

--- think.c
+++ think.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Morning pass wonder you marriage kid establish.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
