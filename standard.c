cc56fa6a12ab95d097a8dcdff231001b6ffdfa98
sho88s <df93hc@outlook.com>
2024-11-30 23:43:18 +0800

    Modify standard.c

--- standard.c
+++ standard.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Concern throw television style soldier determine.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
