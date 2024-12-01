177f53a40c67343ed7b180d59d71a8261b5a95a3
sho88s <df93hc@outlook.com>
2024-12-02 00:13:01 +0800

    Modify bank.c

--- bank.c
+++ bank.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Society suggest within myself gas particular and.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
