33206a93e2d60f6e808220021803b42d82c22f27
sho88s <df93hc@outlook.com>
2024-12-11 06:18:37 +0800

    Modify alone.c

--- alone.c
+++ alone.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Him eight compare within own where return.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
