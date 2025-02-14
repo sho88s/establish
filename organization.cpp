994aa3aaa56d0b81f683f9523d934543982a961d
sho88s <df93hc@outlook.com>
2025-02-14 12:22:00 +0800

    Modify organization.cpp

--- organization.cpp
+++ organization.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Player feel price power tonight spend.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
