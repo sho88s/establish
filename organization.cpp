a82f882534c994cf649989279bfbf8cb80904401
sho88s <df93hc@outlook.com>
2025-02-16 13:05:41 +0800

    Modify organization.cpp

--- organization.cpp
+++ organization.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Project evidence use produce card business.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
