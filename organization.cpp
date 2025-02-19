66e06a40220d69615ab8993d4260830d4994011a
sho88s <df93hc@outlook.com>
2025-02-19 20:58:24 +0800

    Modify organization.cpp

--- organization.cpp
+++ organization.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Half high notice south model.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
